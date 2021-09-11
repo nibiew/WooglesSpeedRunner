c8a9bb46833f8ac6dd0d20e707ec48707c295d07
nibiew <tohweibin90@gmail.com>
2021-09-11 20:39:26 +0800

    Added multiple bot options

--- README.md
+++ README.md
@@ -3,13 +3,17 @@ A tool that facilitates Woogles bot "speed runs" - i.e. first to reach x wins ag
 
 ## Instructions
 
-	1. Make sure you have macondog.png downloaded in the same directory.
+	1. Make sure you have macondog.png downloaded in the same directory as the .py script or .exe file.
 	
-	2. Key in your Woogles username, select the correct time control, bot, lexicon and challenge rule for the run, and key in the number of wins to achieve.
+	2. Key in your Woogles username, select the correct time control, lexicon and challenge rule for the run.
 	
-	3. Click "Start run!"
+	3. You can select a bot and key in the number of wins to achieve. Alternatively, select 'Use config file' if you would like to run multiple bots. If you take this option, config.txt needs to be placed in the same directory and filled with just the bot names, one per line (case-insensitive).
 	
-	4. When you have completed the run, a .json file will be created in the same directory with the filename "run_<start_time>.json". This follows the Splits.io Exchange Format and can be uploaded to https://splits.io/.
+	4. Select 'No fails' if this is a no-fail run, i.e. run fails if you lose against the bot at any juncture.
+	
+	5. Click "Start run!". Do this before you start the game against the bot.
+	
+	6. When you have completed the run, a .json file will be created in the same directory with the filename "run_<start_time>.json". This follows the Splits.io Exchange Format and can be uploaded to https://splits.io/.
 
 ## General Notes
 
@@ -23,8 +27,6 @@ A tool that facilitates Woogles bot "speed runs" - i.e. first to reach x wins ag
 	
 	5. Suggested speed run rules: no resignations, no passing unless necessary.
 	
-## Possible future improvements (all quite doable, just laborious)
+## Possible future improvements (doable, just laborious)
 
-	1. Allow people to run against multiple bots.
-	
-	2. Allow time controls to be defined specifically, e.g. 15 seconds with increment, rather than just "Ultrablitz".
+	1. Allow time controls to be defined specifically, e.g. 15 seconds with increment, rather than just "Ultrablitz".
--- WooglesSpeedRunner.py
+++ WooglesSpeedRunner.py
@@ -14,10 +14,24 @@ def check_every_n_seconds(n=5):
     games = []
     won_games = []
     segments = []
-    wins_required = int(values['-WINS-'])
     spread = 0
     end_time = run_start_time #baseline
-    while len(won_games) < wins_required:
+    if values["-CONFIG-"] == True: #using a config file
+        #read config.txt file
+        config = open("config.txt", "r")
+        wins_to_complete = []
+        for line in config:
+            stripped_line = line.strip()
+            wins_to_complete.append(stripped_line.capitalize())
+        config.close()
+        bot = ', '.join(wins_to_complete).replace('b','B').replace('Stee','STEE')
+    else:
+        wins_to_complete = [str(values['-BOT-'])] * int(values['-WINS-'])
+        bot = str(values['-BOT-'])
+    wins_required = str(len(wins_to_complete)) + " win" + "s" if int(values['-WINS-']) > 1 else ""
+    window['-RUN-'].update(f"Run to {wins_required} - {str(values['-LEXICON-'])} {bot} {str(values['-TIME-'])} {str(values['-CHALLENGE-'])}", text_color = 'red')
+    
+    while len(wins_to_complete) > 0:
         print('New check')
         r = client.post(URL, json = headers)
         game_info = json.loads(r.content)['game_info'][0]
@@ -35,7 +49,7 @@ def check_every_n_seconds(n=5):
                     run_total_time = (end_time - run_start_time).total_seconds()
                     run_split_time = run_total_time - run_old_time
                     print('Correct time setting')
-                    if game_info['players'][0]['nickname'] == str(values['-BOT-']): #if the bot is the first player
+                    if game_info['players'][0]['nickname'].lower() == wins_to_complete[0].lower(): #if the bot is the first player
                         print('Bot is player 0')
                         game_split_time = (end_time - start_time).total_seconds()
                         game_total_time = game_total_time + game_split_time
@@ -44,10 +58,15 @@ def check_every_n_seconds(n=5):
                         if game_info['winner'] == 1: #player won; write split
                             print('game won')
                             won_games.append(game_id)
+                            wins_to_complete.pop(0)
                             segments.append({"name": f"Win {len(won_games)}", "endedAt": {"realtimeMS": run_total_time*1000, "gametimeMS": game_total_time*1000}})
                             window['-SPLITS-' + sg.WRITE_ONLY_KEY].print(f"Win {len(won_games)}")
                             sg.cprint(f">Game time: {str(timedelta(seconds=game_split_time))}\n>Total: {str(timedelta(seconds=game_total_time))}\n>Run time: {str(timedelta(seconds=run_split_time))}\n>Total: {str(timedelta(seconds=run_total_time))}", text_color="grey")
-                    elif game_info['players'][1]['nickname'] == str(values['-BOT-']): #if the bot is the second player
+                        else: #player lost; trigger nofail condition
+                            print('game lost')
+                            if values["-NOFAIL-"] == True: #using a config file
+                                break
+                    elif game_info['players'][1]['nickname'].lower() == wins_to_complete[0].lower(): #if the bot is the second player
                         print('Bot is player 1')
                         game_split_time = (end_time - start_time).total_seconds()
                         game_total_time = game_total_time + game_split_time
@@ -56,9 +75,14 @@ def check_every_n_seconds(n=5):
                         if game_info['winner'] == 0: #player won; write split
                             print('game won')
                             won_games.append(game_id)
+                            wins_to_complete.pop(0)
                             segments.append({"name": f"Win {len(won_games)}", "endedAt": {"realtimeMS": run_split_time*1000, "gametimeMS": game_split_time*1000}})
                             window['-SPLITS-' + sg.WRITE_ONLY_KEY].print(f"Win {len(won_games)}")
                             sg.cprint(f">Game time: {str(timedelta(seconds=game_split_time))}\n>Total: {str(timedelta(seconds=game_total_time))}\n>Run time: {str(timedelta(seconds=run_split_time))}\n>Total: {str(timedelta(seconds=run_total_time))}", text_color="grey")
+                        else: #player lost; trigger nofail condition
+                            print('game lost')
+                            if values["-NOFAIL-"] == True: #using a config file
+                                break
                     else:
                         window['-SPLITS-' + sg.WRITE_ONLY_KEY].print('Wrong opponent')
                     window['-STATS-'].update(f"Wins: {len(won_games)}/{len(games)} Spread: %s{spread}"%("+" if spread >= 0 else ""))
@@ -67,35 +91,36 @@ def check_every_n_seconds(n=5):
             else:
                 print('No new game completed')
         time.sleep(n - ((datetime.now(timezone.utc) - run_start_time).total_seconds() % n)) #check every 5 seconds
-    else: #executes at the end of the run
-        started=False
-        window['-SPLITS-' + sg.WRITE_ONLY_KEY].print(f"Run is complete! Saved to run_{run_start_time.strftime('%m_%d_%Y_%H_%M_%S')}.json")
-        window['-RUN-'].update('Run is complete!', text_color = 'white')
-        data = { "_schemaVersion": "v1.0.1", 
-            "timer": { "shortname": "woogles", "longname":  "Woogles Speedrunner","version":   "v1.0.0", "website": "https://github.com/nibiew/WooglesSpeedRunner" },
-            "videoURL": "",
-            "startedAt": run_start_time.isoformat(),
-            "endedAt": end_time.isoformat(),
-            "game": {"longname": "Woogles.io"},
-            "category": {"longname": f"{str(values['-LEXICON-'])} {str(values['-BOT-'])} {str(values['-TIME-'])} {str(values['-CHALLENGE-'])} - Run to {str(values['-WINS-'])} win%s"%("s" if int(values['-WINS-']) > 1 else "")},
-            "segments": segments
-        }
-        with open(f"run_{run_start_time.strftime('%m_%d_%Y_%H_%M_%S')}.json", 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+    #executes at the end of the run
+    started=False
+    window['-SPLITS-' + sg.WRITE_ONLY_KEY].print(f"Run %s! Saved to run_{run_start_time.strftime('%m_%d_%Y_%H_%M_%S')}.json"%('is complete' if len(wins_to_complete) == 0 else 'failed'))
+    window['-RUN-'].update(f"Run %s!"%('is complete' if len(wins_to_complete) == 0 else 'failed'), text_color = 'white')
+    data = { "_schemaVersion": "v1.0.1", 
+        "timer": { "shortname": "woogles", "longname":  "Woogles Speedrunner","version":   "v1.0.0", "website": "https://github.com/nibiew/WooglesSpeedRunner" },
+        "videoURL": "",
+        "startedAt": run_start_time.isoformat(),
+        "endedAt": end_time.isoformat(),
+        "game": {"longname": "Woogles.io"},
+        "category": {"longname": f"{str(values['-LEXICON-'])} {bot} {str(values['-TIME-'])} {str(values['-CHALLENGE-'])} - Run to {wins_required}"},
+        "segments": segments
+    }
+    with open(f"run_{run_start_time.strftime('%m_%d_%Y_%H_%M_%S')}.json", 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 # Define the window's contents
 layout = [[sg.Column([[sg.Text('Woogles Username')], [sg.InputText(key='-ID-', size=(30,1))],
     [sg.Text('Time control'), sg.Combo(['Ultrablitz', 'Blitz', 'Rapid', 'Regular'], default_value='Ultrablitz',key='-TIME-')],
-    [sg.Text('Bot'), sg.Combo(['HastyBot', 'STEEBot', 'BetterBot', 'BasicBot', 'BeginnerBot'], default_value='HastyBot', key='-BOT-')],
     [sg.Text('Lexicon'), sg.Combo(['CSW19', 'NWL20', 'ECWL', 'RD28', 'FRA20', 'NSF21'], default_value='CSW19', key='-LEXICON-', size=(8,1))],
     [sg.Text('Challenge Rule'), sg.Combo(['FIVE_POINT', 'TEN_POINT', 'DOUBLE', 'SINGLE', 'VOID', 'TRIPLE'], default_value='FIVE_POINT', key='-CHALLENGE-', size=(12,1))],
+    [sg.Text('Bot'), sg.Combo(['HastyBot', 'STEEBot', 'BetterBot', 'BasicBot', 'BeginnerBot'], default_value='HastyBot', key='-BOT-')],
     [sg.Text('Number of wins'), sg.InputText(key='-WINS-', size=(4,1), enable_events=True, default_text='5')],
+    [sg.Checkbox('Use config file', key='-CONFIG-'), sg.Checkbox('No fails', key='-NOFAIL-')],
     [sg.Button('Start run!')]]), 
     sg.Column([[sg.Image(key='-IMAGE-', filename="macondog.png", size=(200,100))]])],
-    [sg.Column([[sg.Text('Run has not started', size=(20,1), key='-RUN-')],
-    [sg.Text('Wins: 0/0 Spread: +0', size=(20,1), key='-STATS-')]]),
+    [sg.Column([[sg.Text('Wins: 0/0 Spread: +0', size=(25,1), key='-STATS-')],
+    [sg.Text('Run has not started', size=(25,3), key='-RUN-')]]),
     sg.Column([[sg.Text('0:00:00.000000', key='-STOPWATCH-', size=(14,1), font=("Helvetica", 25))]])],
-    [sg.MLine(size=(60,21), disabled=True, key='-SPLITS-'+ sg.WRITE_ONLY_KEY)]] #height is 11 because there is an empty line at the end
+    [sg.MLine(size=(60,11), disabled=True, key='-SPLITS-'+ sg.WRITE_ONLY_KEY)]] #height is 11 because there is an empty line at the end
 
 # Create the window
 global window
@@ -117,7 +142,6 @@ while True:
         if started == True:
             sg.popup('Run already started!')
             continue
-        started = True
         client = requests.session()
         headers = {
                      'username' : str(values['-ID-']),
@@ -131,12 +155,18 @@ while True:
         except:
             sg.popup('Invalid user!')
             continue
-        window['-RUN-'].update('Run has started!', text_color = 'red')
+        started = True
         window['-STATS-'].update('Wins: 0/0 Spread: +0')
         thread = threading.Thread(target=check_every_n_seconds, daemon=True)
         thread.start()
     elif event == '-WINS-' and values['-WINS-'] and (values['-WINS-'][-1] not in ('0123456789') or len(values['-WINS-'])>3):
         window['-WINS-'].update(values['-WINS-'][:-1])
+    if values["-CONFIG-"] == True:
+        window.FindElement('-BOT-').Update(disabled = True)
+        window.FindElement('-WINS-').Update(disabled = True)
+    elif values['-CONFIG-'] == False:
+        window.FindElement('-BOT-').Update(disabled = False)
+        window.FindElement('-WINS-').Update(disabled = False)
     if started == True:
         stopwatch_time_seconds = (datetime.now(timezone.utc) - run_start_time).total_seconds()
         stopwatch_time = str(timedelta(seconds=stopwatch_time_seconds))
